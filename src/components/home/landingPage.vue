1fffd0c8e176bec09cd9003ab4db2a47ec19ae03
vikash2806 <vikashrajendran2806@gmail.com>
2021-11-10 22:41:19 +0530

    parallax new images added

--- src/components/home/landingPage.vue
+++ src/components/home/landingPage.vue
@@ -1,6 +1,6 @@
 <template>
-    <main class="main">
-        <div data-aos="zoom-in" data-aos-duration="1000"  class="home-text">
+    <main  class="main">
+        <div data-aos="zoom-in" data-aos-duration="1000" class="home-text">
             <h1 class="greet-text">🙏Vanakkam,</h1>
             <h1>I'am <strong>Vikash</strong></h1>
             <p class="design-text">Student / Web Developer</p>
@@ -9,6 +9,20 @@
             <div class="blob" style="">
                 <img src="@/assets/vikash-min.jpeg" alt="vikash-landing page image">
             </div>
+        </div>
+        <div class="parallax-bg" >
+            <img  src="@/assets/bg-imgs/1.png" alt="">
+            <img  src="@/assets/bg-imgs/2.png" alt="">
+            <img  src="@/assets/bg-imgs/3.png" alt="">
+            <img  src="@/assets/bg-imgs/4.png" alt="">
+            <img  src="@/assets/bg-imgs/5.png" alt="">
+            <img  src="@/assets/bg-imgs/6.png" alt="">
+            <img  src="@/assets/bg-imgs/7.png" alt="">
+            <img  src="@/assets/bg-imgs/8.png" alt="">
+            <img  src="@/assets/bg-imgs/9.png" alt="">
+            <img  src="@/assets/bg-imgs/10.png" alt="">
+            <img  src="@/assets/bg-imgs/11.png" alt="">
+            <img  src="@/assets/bg-imgs/12.png" alt="">
         </div>
     </main>
 </template>
@@ -19,31 +33,58 @@
 </script>
 
 <style scoped>
-.main{
-    height: 100vh;
-    width: 90%;
-    display: flex;
-    align-items: center;
-    justify-content:space-evenly;
-}
-/* NOTE Text */
-.home-text{
-    margin-inline: 10px;
-}
-.home-text h1{
-    font-size:5rem;
-    font-weight: 500;
-}
-.home-text .greet-text{
-    color: rgb(236, 236, 5);
-}
-.home-text h1 strong{
-    color: rgb(250, 36, 121);
-}
-.design-text{
-    font-size:1.2rem;
-}
-/* NOTE Blob */
+    .main {
+        height: 100vh;
+        width: 90%;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+
+    /* Parallax bg */
+    .parallax-bg {
+        position: absolute;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: -1;
+        opacity: 1;
+    }
+
+    .parallax-bg img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+    }
+
+    /* NOTE Text */
+    .home-text {
+        margin-inline: 10px;
+    }
+
+    .home-text h1 {
+        font-size: 5rem;
+        font-weight: 500;
+    }
+
+    .home-text .greet-text {
+        color: rgb(236, 236, 5);
+    }
+
+    .home-text h1 strong {
+        color: rgb(250, 36, 121);
+    }
+
+    .design-text {
+        font-size: 1.2rem;
+    }
+
+    /* NOTE Blob */
     .wrap {
         display: flex;
         align-items: center;
@@ -53,25 +94,26 @@
 
     .blob {
         width: 85%;
-        max-width:550px;
-        min-width:200px;
+        max-width: 550px;
+        min-width: 200px;
         border-radius: 40% 60% 70% 30% / 40% 50% 60% 50%;
         overflow: hidden;
         animation: morph 3s linear infinite;
         transition: all 0.4s ease;
         border: 4px solid rgb(250, 36, 121);
-        box-shadow: -1px -1px 36px 10px rgba(250,36,121,0.51);
-        -webkit-box-shadow: -1px -1px 36px 10px rgba(250,36,121,0.51);
-        -moz-box-shadow: -1px -1px 36px 10px rgba(250,36,121,0.51);
+        box-shadow: -1px -1px 36px 10px rgba(250, 36, 121, 0.51);
+        -webkit-box-shadow: -1px -1px 36px 10px rgba(250, 36, 121, 0.51);
+        -moz-box-shadow: -1px -1px 36px 10px rgba(250, 36, 121, 0.51);
     }
-    .blob:hover img{
-        transform:scale(1.2);
+
+    .blob:hover img {
+        transform: scale(1.2);
         opacity: 1;
     }
 
     .blob img {
         transition: all 0.4s ease;
-        opacity: 0.6;
+        opacity: 0.9;
         width: 100%;
     }
 
@@ -90,31 +132,36 @@
             border-radius: 100% 60% 60% 100% / 100% 100% 60% 60%;
         }
     }
+
     @media only screen and (min-width: 1200px) {
-          .blob {
-          width: 500px !important;
-      }
+        .blob {
+            width: 500px !important;
+        }
     }
+
     @media only screen and (min-width: 992px) {
         .blob {
-          width: 400px;
-      }
+            width: 400px;
+        }
     }
-@media only screen and (min-width: 768px) {
-      .blob {
-          width: 300px;
-      }
-}
-  @media (max-width: 786px) {
-      .main{
-          flex-direction: column;
-          width: 100%;
-          justify-content:space-evenly;
-          align-items: center;
-      }
-      .blob img{
-          opacity:1;
-          transform:scale(1.1);
-      }
-  }
+
+    @media only screen and (min-width: 768px) {
+        .blob {
+            width: 300px;
+        }
+    }
+
+    @media (max-width: 786px) {
+        .main {
+            flex-direction: column;
+            width: 100%;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+
+        .blob img {
+            opacity: 1;
+            transform: scale(1.1);
+        }
+    }
 </style>
